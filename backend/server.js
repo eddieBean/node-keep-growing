f4865c718e0428b591d04ca677f0a68b214f69fc
eddieBean <ed.beaman@outlook.com>
2026-04-27 21:44:10 +1000

    trying to fix server connections

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true })); // parses HTML form submissions
 app.use(express.json());                          // parses JSON requests
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
 }));
 
 
@@ -23,85 +23,92 @@ app.use(express.static(path.join(__dirname, '..', 'frontend')));
 app.use('/images', express.static(path.join(__dirname, '..', 'frontend', 'images')));
 
 
-app.post('/signup', async (req, res) =>{
-  
-  const {email, password, phone} = req.body;
-  console.log(req.body.email, req.body.password, req.body.phone);
-  try {
-    //test if the email or phone number has already been used
-    const[existingEmail] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
-    if (existingEmail.length > 0){
-      return res.send('An account with that email already exists')
-    }
-    const[existingPhone] = await db.query('SELECT * FROM users WHERE phone = ?', [phone]);
-    if (existingPhone.length > 0){
-      return res.send('An account with that phone number already exists')
-    }
-    //hash the password before storing it in the db
-    const hashedPassword = await bcrypt.hash(password, 10);
+app.post('/signup', async (req, res) => {
 
-    await db.query(
-      'INSERT INTO users (email, password, phone) VALUES (?, ?, ?)',
-      [email, hashedPassword, phone]
-    );kjg
-    req.session.userEmail = email;
-    res.redirect('/');
+    const { email, password, phone } = req.body;
+    console.log(req.body.email, req.body.password, req.body.phone);
+    try {
+        //test if the email or phone number has already been used
+        const [existingEmail] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
+        if (existingEmail.length > 0) {
+            return res.send('An account with that email already exists')
+        }
+        const [existingPhone] = await db.query('SELECT * FROM users WHERE phone = ?', [phone]);
+        if (existingPhone.length > 0) {
+            return res.send('An account with that phone number already exists')
+        }
+        //hash the password before storing it in the db
 
-  } catch (err) {
-    console.error('Signup error', err.message);
-    res.status(500).send('Signup failed')
-  }
+        bcrypt.genSalt(10, function (err, salt) {
+            bcrypt.hash(password, salt, function (err, hash) {
+                console.log("here");
+                var hashedPassword = hash;
+
+            });
+        });
+
+        db.query(
+            'INSERT INTO users (email, password, phone) VALUES (?, ?, ?)',
+            [email, hashedPassword, phone]
+        );
+        req.session.userEmail = email;
+        res.redirect('/');
+
+    } catch (err) {
+        console.error('Signup error', err.message);
+        res.status(500).send('Signup failed')
+    }
 });
 
-app.post('/login', async (req, res) =>{
-  const { email, password} = req.body;
-  console.log("request: "+req.body.email + req.body.password);
-  try {
-    const [rows] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
-    if(rows.length === 0){
-      return res.send('no account with that email exists')
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    console.log("request: " + req.body.email + req.body.password);
+    try {
+        const [rows] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
+        if (rows.length === 0) {
+            return res.send('no account with that email exists')
+        }
+
+        const user = rows[0];
+        console.log("db response: " + user.email + user.password);
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res.send('Incorrect Password');
+        }
+
+        req.session.userEmail = email;
+        res.redirect('/frontend/index.html');
+
+    } catch (err) {
+        console.error('Signup error', err.message);
+        res.status(500).send('Signup failed')
     }
-
-    const user = rows[0];
-    console.log("db response: "+user.email+user.password);
-
-    const match = await bcrypt.compare(password, user.password);
-    if (!match){
-      return res.send('Incorrect Password');
-    }
-
-    req.session.userEmail = email;
-    res.redirect('/frontend/index.html');
-
-  } catch (err) {
-    console.error('Signup error', err.message);
-    res.status(500).send('Signup failed')
-  }
 });
 
 app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.redirect('/');
+    req.session.destroy();
+    res.redirect('/');
 })
 
 // API endpoint returning items as JSON
 app.get('/api/items', async (req, res) => {
-  console.log(req.query.item)
-  try {
-    const [rows] = await db.query('SELECT * FROM items');
-    res.json(rows);
-  } catch (err) {
-    console.error('Database error:', err.message);
-    res.status(500).json({ error: 'Database error' });
-  }
+    console.log(req.query.item)
+    try {
+        const [rows] = await db.query('SELECT * FROM items');
+        res.json(rows);
+    } catch (err) {
+        console.error('Database error:', err.message);
+        res.status(500).json({ error: 'Database error' });
+    }
 });
 
 app.get('/api/session', (req, res) => {
-  if (req.session.user) {
-    res.json({ loggedIn: true, user: req.session.user });
-  } else {
-    res.json({ loggedIn: false });
-  }
+    if (req.session.user) {
+        res.json({ loggedIn: true, user: req.session.user });
+    } else {
+        res.json({ loggedIn: false });
+    }
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
