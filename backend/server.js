1a572e6ecd73c4731e619dd0b8b44c655f561803
eddieBean <ed.beaman@outlook.com>
2026-05-05 06:28:07 +1000

    item listing, posting and emailinjg functional

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 const db = require('./db');
-
+const e = require('express');
+const fileUpload = require('express-fileupload');
+const nodemailer = require('nodemailer');
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true })); // parses HTML form submissions
@@ -15,7 +17,17 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }));
+app.use(fileUpload({
+    safeFileNames: /[<>"'&]/g
+}));
 
+const emailer  = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
 
 
 //serve data from pages specifically
@@ -45,6 +57,7 @@ app.post('/signup', async (req, res) => {
             'INSERT INTO users (email, password, phone) VALUES (?, ?, ?)',
             [email, hashedPassword, phone]
         );
+        req.session.userId = user.user_id;
         req.session.userEmail = email;
         res.json({ success: true, message: 'Signup successful' });
 
@@ -70,6 +83,7 @@ app.post('/login', async (req, res) => {
         if (!match) {
             return res.status(400).json({ success: false, message: 'Incorrect Password' });
         }
+        req.session.userId = user.user_id;
         req.session.userEmail = email;
         res.json({ success: true, message: 'Login successful' });
 
@@ -78,6 +92,7 @@ app.post('/login', async (req, res) => {
         res.status(500).json({ success: false, message: 'Login failed' })
     }
 });
+
 
 app.get('/logout', (req, res) => {
     console.log("logging out");
@@ -119,11 +134,113 @@ app.get('/api/items/itemId/:itemId', async (req, res) => {
 });
 
 
+app.post('/api/items/upload', async (req, res) => {
+
+    console.log('[UPLOAD/IMAGE] Request received');
+    if (!req.files || Object.keys(req.files).length === 0) {
+        console.log('[UPLOAD/IMAGE] No files provided in request');
+        return res.status(500).json({ success: false, message: 'No file uploaded' });
+    }
+
+    const uploadedImage = req.files.image;
+    console.log(`[UPLOAD/IMAGE] File received: ${uploadedImage.name}, Size: ${uploadedImage.size} bytes`);
+
+    const uploadPath = path.join(__dirname, '..', 'frontend', 'images', uploadedImage.name);
+    console.log(`[UPLOAD/IMAGE] Target path: ${uploadPath}`);
+    const imageUrl = uploadedImage.name;
+
+
+
+    const { name, description, price, condition, size, ISBN, image_url, owner_id} = req.body;
+    console.log('[UPLOAD/ITEM] Request received with data:', { name, description, price, condition, size, ISBN, image_url, owner_id });
+    try {
+        console.log('[UPLOAD/ITEM] Inserting item into database...');
+        await db.query(
+            'INSERT INTO items (`name`, `description`, `price`, `condition`, `size`, `isbn`, `image`, `owner_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [name, description, price, condition, size, ISBN, image_url, owner_id]
+        );
+        console.log('[UPLOAD/ITEM] Success - Item inserted into database');
+        try {
+            await uploadedImage.mv(uploadPath);
+            console.log(`[UPLOAD/IMAGE] Success - File saved to: ${uploadPath}, URL: ${imageUrl}`);
+        } catch (err) {
+            console.error(`[UPLOAD/IMAGE] Error moving file: ${err.message}`);
+            await db.query('DELETE FROM items WHERE `name` = ? AND `description` = ? AND `price` = ? AND `condition` = ? AND `size` = ? AND `isbn` = ? AND `image` = ? AND `owner_id` = ?',
+            [name, description, price, condition, size, ISBN, image_url, owner_id]);
+            return res.status(500).json({ success: false, message: 'Image upload failed, item removed from database' });
+        }
+        res.json({ success: true, message: 'Item uploaded successfully' });
+    } catch (err) {
+        console.error('[UPLOAD/ITEM] Database error:', err.message);
+        res.status(500).json({success: "false", error: 'Database error' });
+    }
+
+});
+
+app.post('/api/items/upload/image', async (req, res) => {
+    console.log('[UPLOAD/IMAGE] Request received');
+    if (!req.files || Object.keys(req.files).length === 0) {
+        console.log('[UPLOAD/IMAGE] No files provided in request');
+        return res.status(500).json({ success: false, message: 'No file uploaded' });
+    }
+
+    const uploadedImage = req.files.image;
+    console.log(`[UPLOAD/IMAGE] File received: ${uploadedImage.name}, Size: ${uploadedImage.size} bytes`);
+
+    const uploadPath = path.join(__dirname, '..', 'frontend', 'images', uploadedImage.name);
+    console.log(`[UPLOAD/IMAGE] Target path: ${uploadPath}`);
+
+    try {
+        await uploadedImage.mv(uploadPath);
+        const imageUrl = `/images/${uploadedImage.name}`;
+        console.log(`[UPLOAD/IMAGE] Success - File saved to: ${uploadPath}, URL: ${imageUrl}`);
+        res.json({ success: true, message: 'Image uploaded successfully', imageUrl: imageUrl });
+
+    } catch (err) {
+        console.error(`[UPLOAD/IMAGE] Error moving file: ${err.message}`);
+        res.status(500).json({ success: false, message: 'Image upload failed' });
+    }
+});
+
+app.post('/sendEmail', async (req, res) => {
+    const { senderId, recipientId, itemId, content } = req.body;
+    console.log("message request received with data: " + senderId + recipientId + itemId + content);
+    let recipient, sender, item;
+    try {
+        const [recipientRows] = await db.query('SELECT email FROM users WHERE user_id = ?', [recipientId]);
+        const [senderRows] = await db.query('SELECT email, user_name, phone FROM users WHERE user_id = ?', [senderId]);
+        const [itemRows] = await db.query('SELECT name FROM items WHERE item_id = ?', [itemId]);
+        if (recipientRows.length === 0 || senderRows.length === 0 || itemRows.length === 0) {
+            return res.status(400).json({ success: false, message: 'Invalid sender, recipient, or item ID' });
+        }
+        recipient = recipientRows[0];
+        sender = senderRows[0];
+        item = itemRows[0];
+    }catch (err) {
+        console.error('Database error:', err.message);
+        return res.status(500).json({ success: false, message: '[SEND-EMAIL] Database error' });
+    }
+    const mail = {
+        from: process.env.EMAIL_USER,
+        to: recipient.email,
+        subject: `Message from user "${sender.user_name}" about your listing "${item.name}"`,
+        text: `${sender.user_name}'s message:\n ${content} +\n\nReply to ${sender.user_name} via text on ${sender.phone} to respond to this offer.`
+    }
+    console.log(JSON.stringify(mail));
+    res.json({ success: true, message: 'Email sent successfully' });
+    // emailer.sendMail(mail, (err, info) => {
+    //     if (err) {
+    //         console.error('Error sending email:', err.message);
+    //     } else {
+    //         console.log('Email sent:', info.response);
+    //     }
+});
+
 app.get('/api/session', (req, res) => {
-    console.log(req.session.userEmail);
+    // console.log(req.session.userEmail);
     if (typeof req.session.userEmail === 'string') {
         console.log("logged in");
-        res.json({ loggedIn: true, user: req.session.userEmail });
+        res.json({ loggedIn: true, user: req.session.userEmail, userId: req.session.userId });
     } else {
         console.log("logged out");
         res.json({ loggedIn: false });
